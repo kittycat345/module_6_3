dadfd95150a2e9e0a0c873b81121ba43a247e552
kittycat345 <ilyapavl0v531@gmail.com>
2024-07-15 13:26:42 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,40 @@
+class Horse:
+    sound = 'Frrr'
+
+    x_distance = 0
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self):
+
+        sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Eagle, Horse):
+
+    def move(self, dx, dy):
+        super().fly(dy)
+        super().run(dx)
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
